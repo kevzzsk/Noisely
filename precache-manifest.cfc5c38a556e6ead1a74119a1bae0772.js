89bffcb193e0d0bb5016e20eb2da4518f40e6acf
KevinSk <kevi0023@e.ntu.edu.sg>
2019-08-05 00:53:30 +0800

    Updates

---
+++ precache-manifest.cfc5c38a556e6ead1a74119a1bae0772.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "171f99be91c7c9c5a6838107417d6ea6",
+    "url": "/Noisely/index.html"
+  },
+  {
+    "revision": "659eb9cdbac75700cd92",
+    "url": "/Noisely/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "c92cbc93bececb617db0",
+    "url": "/Noisely/static/css/main.afde3df8.chunk.css"
+  },
+  {
+    "revision": "659eb9cdbac75700cd92",
+    "url": "/Noisely/static/js/2.44a8cb37.chunk.js"
+  },
+  {
+    "revision": "c92cbc93bececb617db0",
+    "url": "/Noisely/static/js/main.b0184338.chunk.js"
+  },
+  {
+    "revision": "29f9d521b8c6d1cbe2ea",
+    "url": "/Noisely/static/js/runtime~main.012eabec.js"
+  },
+  {
+    "revision": "ed77ed81c3b369007438ed5d917fc97a",
+    "url": "/Noisely/static/media/Album.ed77ed81.jpeg"
+  }
+]);
